af3719f99a01f1e0a1d20fc3a375b500ce0ec95f
Rajjoshi77 <110289049+Rajjoshi77@users.noreply.github.com>
2025-06-09 17:25:54 +0530

    Update client_test.py

--- client_test.py
+++ client_test.py
@@ -8,18 +8,48 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+  for quotes in quotes:
+      self.assertEqual(getDataPoint(quotes), (quotes['stock'], quotes['top_bid']['price'], quotes['top_ask']['price'], (quotes['top_bid']['price'] + quotes['top_ask']['price']) / 2))
+  
+ def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+for quotes in quotes:
+      self.assertEqual(getDataPoint(quotes), (quotes['stock'], quotes['top_bid']['price'], quotes['top_ask']['price'], (quotes['top_bid']['price'] + quotes['top_ask']['price']) / 2))
 
   """ ------------ Add more unit tests ------------ """
 
-
+  def test_getDataPoint_zeroPrices(self):
+      quote = {
+          'top_ask': {'price': 0.0, 'size': 36},
+          'top_bid': {'price': 0.0, 'size': 109},
+          'timestamp': '2019-02-11 22:06:30.572453',
+          'id': '0.4', 'stock': 'LMN'
+      }
+      expected_price = 0.0
+      self.assertEqual(getDataPoint(quote), ('LMN', 0.0, 0.0, expected_price))
+    
+def test_getDataPoint_missingAsk(self):
+        quote = {
+            'top_ask': {'size': 36},  # price missing
+            'top_bid': {'price': 120.0, 'size': 109},
+            'timestamp': '2019-02-11 22:06:30.572453',
+            'id': '0.5', 'stock': 'ERR'
+        }
+        with self.assertRaises(KeyError):
+            getDataPoint(quote)
+  def test_getDataPoint_nonNumericPrices(self):
+      quote = {
+          'top_ask': {'price': 'abc', 'size': 36},
+          'top_bid': {'price': 120.0, 'size': 109},
+          'timestamp': '2019-02-11 22:06:30.572453',
+          'id': '0.6', 'stock': 'BAD'
+      }
+      with self.assertRaises(ValueError):
+          getDataPoint(quote)
 
 if __name__ == '__main__':
     unittest.main()
